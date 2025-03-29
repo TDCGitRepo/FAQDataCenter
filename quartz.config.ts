bf7f10e087741960d4f3efe4f06e7ff2ba1906cb
DocHudson45 <dzakyrryrdi@gmail.com>
2025-03-29 11:22:54 +0700

    Quartz sync: Mar 29, 2025, 11:22 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -35,7 +35,7 @@ const config: QuartzConfig = {
           darkgray: "#4e4e4e",
           dark: "#060606", 
           secondary: "#cd1414", 
-          tertiary: "#cd1414",
+          tertiary: "#f1efee",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#fff23688",
         },
